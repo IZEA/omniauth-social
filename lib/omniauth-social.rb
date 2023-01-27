7f8d717f444771903a0fd394131cb298c60d3f63
ritaamritkar1212 <rita.amritkar@gmail.com>
2023-01-27 16:26:53 +0400

    require updated for a facebook folder

--- lib/omniauth-social.rb
+++ lib/omniauth-social.rb
@@ -2,4 +2,4 @@ require "omniauth/social/version"
 require 'omniauth/strategies/tiktok'
 require 'omniauth/strategies/google_oauth2'
 require 'omniauth/strategies/twitter'
-require 'omniauth/facebook'
+require 'omniauth/facebook/signed_request'
