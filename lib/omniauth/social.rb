be16f34292245be41d255c1575e15e90706350ca
ritaamritkar1212 <rita.amritkar@gmail.com>
2023-01-27 16:04:02 +0400

    added omniauth-social file

--- lib/omniauth/social.rb
+++
@@ -1,5 +0,0 @@
-require "omniauth/social/version"
-require 'omniauth/strategies/tiktok'
-require 'omniauth/strategies/google_oauth2'
-require 'omniauth/strategies/twitter'
-require 'omniauth/facebook'
