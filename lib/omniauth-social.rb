e38dd9d66798aaed2ecc43c83eecf30250bc26ec
ritaamritkar1212 <rita.amritkar@gmail.com>
2023-01-27 16:10:52 +0400

    corrected version path

--- lib/omniauth-social.rb
+++ lib/omniauth-social.rb
@@ -1,4 +1,4 @@
-require "omniauth-social/version"
+require "omniauth/social/version"
 require 'omniauth/strategies/tiktok'
 require 'omniauth/strategies/google_oauth2'
 require 'omniauth/strategies/twitter'
