62ee58ce91dce78a1f58529b8bf634f0235a693c
Abideen Adelu <adelu4real@gmail.com>
2025-04-13 21:12:47 +0100

    Instagram business strategy

--- lib/omniauth/strategies/instagram_business.rb
+++ lib/omniauth/strategies/instagram_business.rb
@@ -5,14 +5,18 @@ module OmniAuth
     # Modified version of: https://github.com/parkerbarker/omniauth-instagram_basic/blob/main/lib/omniauth/strategies/instagram_basic.rb
     # Support for not having media scope
     # expires_at is an absolute timestamp
-    class InstagramBasic < OmniAuth::Strategies::OAuth2
+    class InstagramBusiness < OmniAuth::Strategies::OAuth2
       SITE_URL = 'https://api.instagram.com'.freeze
+      AUTH_URL = 'https://api.instagram.com/oauth/authorize'.freeze
       TOKEN_PATH = 'oauth/access_token'.freeze
       TOKEN_OPTIONS = ["client_id", "client_secret"].freeze
 
-      option :client_options, site: SITE_URL, token_url: TOKEN_PATH, auth_scheme: :request_body
+      option :client_options, site: SITE_URL,
+                              token_url: TOKEN_PATH,
+                              authorization_url: AUTH_URL,
+                              auth_scheme: :request_body
       option :token_options, TOKEN_OPTIONS
-      option :name, 'instagram_basic'
+      option :name, 'instagram_business'
 
       uid { access_token.to_hash["user_id"] }
       extra { { raw_info: raw_info } }
@@ -24,7 +28,7 @@ module OmniAuth
       end
 
       # Instagram provides a short lived token initial with the option to request a long lived token
-      # https://developers.facebook.com/docs/instagram-basic-display-api/guides/long-lived-access-tokens
+      # https://developers.facebook.com/docs/instagram-platform/instagram-api-with-instagram-login/business-login
       def raw_token_info
         url = "https://graph.instagram.com/access_token? \
                 grant_type=ig_exchange_token \
@@ -35,9 +39,9 @@ module OmniAuth
       end
 
       # Get user info
-      # https://developers.facebook.com/docs/instagram-basic-display-api/reference/me
+      # https://developers.facebook.com/docs/instagram-platform/reference/me
       def raw_info
-        url = "https://graph.instagram.com/me? \
+        url = "https://graph.instagram.com/v22.0/me? \
                 fields=id,account_type,username \
                 &access_token=#{access_token.token}"
 
